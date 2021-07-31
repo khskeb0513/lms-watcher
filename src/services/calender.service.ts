e1f17a6bba51cae713c0e11ff8a8533b90170450
hyeonseungKang <khskeb0513@pukyong.ac.kr>
2021-07-31 10:25:59 +0900

    change logic

--- src/services/calender.service.ts
+++ src/services/calender.service.ts
@@ -1,39 +1,97 @@
 import { Injectable } from "@nestjs/common";
-import { SessionService } from "./session.service";
 import got from "got";
 import * as cheerio from "cheerio";
 import * as moment from "moment";
 
 @Injectable()
 export class CalenderService {
-  constructor(private readonly sessionService: SessionService) {
-  }
 
-  public async getCalender(cookie: string) {
+  public async getCalenderByDate(date: string, cookie: string) {
     const body = (
       await got.post(
         "https://lms.pknu.ac.kr/ilos/main/main_schedule_view.acl",
         {
-          headers: { cookie }
+          headers: { cookie },
+          searchParams: { viewDt: date }
         }
       )
     ).body;
     const $ = cheerio.load(body);
-    return $("div.schedule-Detail-Box")
+    return $("div.schedule-show-control")
       .toArray()
       .map((v) => {
-        const dateStr = $(v).children().last().html();
+        const dateStr = $(v).parent().children().last().children().last().text();
+        const title = $(v).text().replace(/ /ig, "").replace(/\n/ig, "");
+        const name = $(v).parent().children().last().children().first().children().first().text()
+          .replace(/ /ig, "").replace(/\n/ig, "");
         if (dateStr.length === 23) {
           const [startDateStr, endDateStr] = dateStr.split(" ~ ");
           return {
-            title: $(v).children().first().html(),
+            title,
             startDate: new Date(startDateStr),
-            endDate: new Date(endDateStr)
+            endDate: new Date(endDateStr),
+            detail: {
+              status: null,
+              name
+            }
           };
-        } else return null;
+        } else {
+          const startDate = new Date(`${date.substr(0, 4)}-${date.substr(4, 2)}-${date.substr(6, 2)}`);
+          const status = $(v).parent().children().last().text();
+          return {
+            title,
+            startDate,
+            endDate: startDate,
+            detail: {
+              status: status.includes("제출") ? (status.includes("미체출") ? "미제출" : "제출") : null,
+              name
+            }
+          };
+        }
       })
       .filter((v) => !!v)
       .sort((a, b) => a.startDate.valueOf() - b.startDate.valueOf());
+  }
+
+  public async getCalender(cookie: string) {
+    const request = async (month: number) => {
+      const body = (await got.post(
+        "https://lms.pknu.ac.kr/ilos/main/main_schedule.acl",
+        {
+          headers: { cookie },
+          searchParams: { month }
+        }
+      )).body;
+      const $ = cheerio.load(body);
+      const children = [];
+      for (let i = 1; i < 32; i++) {
+        const schoolSchedule = $(`#${month}_${i}_0`).length !== 0;
+        const courseSchedule = $(`#${month}_${i}_1`).length !== 0;
+        if (schoolSchedule || courseSchedule) {
+          (await this.getCalenderByDate(
+            `2021${month.toString().length === 1 ? `0${month}` : month}${i.toString().length === 1 ? `0${i}` : i}`,
+            cookie
+          )).forEach(v => {
+            children.push(v);
+          });
+        }
+      }
+      return children;
+    };
+    return [
+      ...await request(1),
+      ...await request(2),
+      ...await request(3),
+      ...await request(4),
+      ...await request(5),
+      ...await request(6),
+      ...await request(7),
+      ...await request(8),
+      ...await request(9),
+      ...await request(10),
+      ...await request(11),
+      ...await request(12)
+    ];
   }
 
   public async getReportCalender(cookie: string) {
