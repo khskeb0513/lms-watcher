b09b68ec36eae8976681df3e55f75e48dd8e67e7
hyeonseungKang <khskeb0513@pukyong.ac.kr>
2021-09-01 01:49:36 +0900

    typo

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -11,7 +11,6 @@ export class IndexController {
   @Get("/")
   @Render("index")
   public async root(@Session() session: Record<string, any>) {
-    session.cookieStr = 'JSESSIONID=1pJuzht6lz98rgdiYqcwijKRJDylXSbawLMU0fyYke9ClWpEpaOKeWurGSF4KFra.UExNUy9wbG1zNjM='
     return {
       username: session.cookieStr ? await this.userService.getUsername(session.cookieStr) : null
     };
